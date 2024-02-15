a4e85d4f81d898cf9684270ed3f32f5782bef38d
Yura Yakovenko <akovenkoura@gmail.com>
2024-02-15 15:12:08 +0200

    initial commit

---
+++ src/main/java/com/example/PersonalAccounting/repositories/AccumulationRepository.java
@@ -0,0 +1,21 @@
+package com.example.PersonalAccounting.repositories;
+
+import com.example.PersonalAccounting.entity.Accumulation;
+import com.example.PersonalAccounting.entity.Transaction;
+import com.example.PersonalAccounting.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface AccumulationRepository extends JpaRepository<Accumulation, Integer> {
+
+    @Query("SELECT a FROM Accumulation a LEFT join FETCH a.user")
+    List<Accumulation> findAll();
+
+    List<Accumulation> findByUserAndStartDateBetween(User user, LocalDate start, LocalDate end);
+
+    List<Accumulation> findByUser(User user);
+}
